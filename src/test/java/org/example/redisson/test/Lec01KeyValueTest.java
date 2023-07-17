dd2cdb5786b668d7a8f1fb6bfa7c00048553cabd
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-07-17 22:11:22 +0700

    1st commit

---
+++ src/test/java/org/example/redisson/test/Lec01KeyValueTest.java
@@ -0,0 +1,22 @@
+package org.example.redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RBucketReactive;
+import org.redisson.client.codec.StringCodec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec01KeyValueTest extends BaseTest{
+
+    @Test
+    public void keyValueAccessTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam");
+        Mono<Void> get = bucket.get()
+                .doOnNext(System.out::println)
+                .then();
+
+        StepVerifier.create(set.concatWith(get))
+                .verifyComplete();
+    }
+}
---
+++ src/test/java/org/example/redisson/test/config/RedissonConfig.java
@@ -0,0 +1,26 @@
+package org.example.redisson.test.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.config.Config;
+
+import java.util.Objects;
+
+public class RedissonConfig {
+    private RedissonClient redissonClient;
+
+    public RedissonClient getRedissonClient() {
+        if (Objects.isNull(this.redissonClient)) {
+            Config config = new Config();
+            config.useSingleServer()
+                    .setAddress("redis://127.0.0.1:6379");
+            redissonClient = Redisson.create(config);
+        }
+        return redissonClient;
+    }
+
+    public RedissonReactiveClient getReactiveClient() {
+        return getRedissonClient().reactive();
+    }
+}
